ead82eaf7f7ec7eb4d2db866dcd800be9b1894e7
Jatin Saini <jatinrajbagri2001@gmail.com>
2024-07-22 04:13:12 +0530

    added longest string prefix check question

--- GFG/JavaScript/Easy/longest-common-prefix-in-an-array.js
+++ GFG/JavaScript/Easy/longest-common-prefix-in-an-array.js
@@ -3,18 +3,18 @@
 class Solution {
   longestCommonPrefix(arr) {
     // code here
-    let commanString = arr[0].length;
+    let commonString = arr[0].length;
 
     for (let i = 0; i < arr.length - 1; i++) {
       let tempString = 0;
       for (let j = 0; j < Math.min(arr[i].length, arr[i + 1].length); j++) {
         if (arr[i][j] != arr[i + 1][j]) {
-          commanString = Math.min(commanString, tempString);
+          commonString = Math.min(commonString, tempString);
         } else {
           tempString++;
         }
       }
     }
-    return commanString > 0 ? arr[0].slice(0, commanString) : -1;
+    return commonString > 0 ? arr[0].slice(0, commonString) : -1;
   }
 }
