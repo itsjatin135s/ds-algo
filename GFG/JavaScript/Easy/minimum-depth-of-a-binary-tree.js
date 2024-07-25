fd71083845dfa95af64fe74c87b99332c000070b
Jatin Saini <jatinrajbagri2001@gmail.com>
2024-07-25 14:40:57 +0530

    added min depth of binary tree in js

---
+++ GFG/JavaScript/Easy/minimum-depth-of-a-binary-tree.js
@@ -0,0 +1,30 @@
+// https://www.geeksforgeeks.org/problems/minimum-depth-of-a-binary-tree/1
+
+class Solution {
+  minDepth(root) {
+    //code here
+    if (!root) {
+      return 0;
+    }
+    let depth = 0;
+    let queue = [root];
+    while (queue.length > 0) {
+      depth++;
+      let levelNodes = queue.length;
+      for (let i = 0; i < levelNodes; i++) {
+        let node = queue.shift();
+
+        if (node.left == null && node.right == null) {
+          return depth;
+        }
+        if (node.left != null) {
+          queue.push(node.left);
+        }
+        if (node.right != null) {
+          queue.push(node.right);
+        }
+      }
+    }
+    return depth;
+  }
+}
