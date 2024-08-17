f2bd65b0489fbb4ca7302819a461edad922480b4
Jatin Saini <jatinrajbagri2001@gmail.com>
2024-08-18 02:52:27 +0530

    added search in matrix question

---
+++ GFG/JavaScript/Easy/search-in-a-matrix.js
@@ -0,0 +1,13 @@
+class Solution {
+  matSearch(matrix, N, M, X) {
+    let height = 0;
+    let width = M - 1;
+
+    while (height <= N - 1 && width >= 0) {
+      if (matrix[height][width] == X) return 1;
+      if (matrix[height][width] > X) width--;
+      if (matrix[height][width] < X) height++;
+    }
+    return 0;
+  }
+}
