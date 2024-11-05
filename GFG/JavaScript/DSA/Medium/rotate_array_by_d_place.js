ef61cbab4d4849f39ce028d8114be1d545f8b297
Jatin Saini <jatinrajbagri2001@gmail.com>
2024-11-06 02:07:23 +0530

    rotate array right optimized

--- GFG/JavaScript/DSA/Medium/rotate_array_by_d_place.js
+++ GFG/JavaScript/DSA/Medium/rotate_array_by_d_place.js
@@ -1,3 +1,4 @@
+// Brute Force
 function rotateArrToRightByDPlace(arr, d) {
   let n = arr.length;
   d = d % n;
@@ -27,9 +28,33 @@ function rotateArrToRightByDPlace(arr, d) {
   return arr;
 }
 
+// Optimized for space complexity
+function rotateArrToRightByDPlaceOptimized(arr, d) {
+  if (d < 1) {
+    return arr;
+  }
+  const n = arr.length - 1;
+  d = d % (n + 1);
+  reverse(arr, 0, n - d);
+  reverse(arr, n - d + 1, n);
+  reverse(arr, 0, n);
+  return arr;
+}
+
+function reverse(arr, start, end) {
+  while (start < end) {
+    temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+  return arr;
+}
+
 test1 = [-1, -100, 3, 99]; // d = 2
 test2 = [1, 2, 3, 4, 5, 6, 7]; //d = 3
 test3 = [1, 2]; //d = 0
 test4 = [1, 2, 3, 4, 5, 6]; //d = 1
-// rotateArrToRightByDPlace(test2, 3);
-console.log(rotateArrToRightByDPlace(test2, 3));
+
+console.log(rotateArrToRightByDPlaceOptimized(test4, 1));
